81bbfcb767a41d8c32ad325e9ffed3e610e2e26b
Fred G <fredg02@hotmail.com>
2016-10-16 14:23:57 +0200

    Refactor tests

--- src/test/java/se/bitcraze/crazyflie/lib/bootloader/TargetTest.java
+++ src/test/java/se/bitcraze/crazyflie/lib/bootloader/TargetTest.java
@@ -9,73 +9,47 @@ public class TargetTest {
 
     @Test
     public void testParseDataSTM32_cf1() {
-        
         //original: 1, -1,-1,16,0,4,10,0,-128,0,10,0,80,-1,118,6,73,-123,86,84,81,38,20,-121,1,0,0,0,0,0,0,0,0
         // removed the first two bytes
         
         byte[] data = new byte[] {-1,16,0,4,10,0,-128,0,10,0,80,-1,118,6,73,-123,86,84,81,38,20,-121,1,0,0,0,0,0,0,0,0};
         Target target = new Target(0xFF);
-        
-        target.parseData(data);
-        
-        System.out.println(target.toString());
-        System.out.println(target.getCpuId());
-        System.out.println();
-        
-        assertEquals(118, target.getAvailableFlash());
-        assertEquals(10, target.getBufferPages());
-        assertEquals(128, target.getFlashPages());
-        assertEquals(255, target.getId());
-        assertEquals(1024, target.getPageSize());
-        assertEquals(1, target.getProtocolVersion()); //0x01
-        assertEquals(10, target.getStartPage());
+        checkParseData(target, data, 118, 10, 128, 255, 1024, 1 /*0x01*/, 10);
     }
 
     @Test
     public void testParseDataSTM32_cf2() {
-
         //original: 1,-1,-1,16,0,4,10,0,0,4,16,0,-89,4,48,106,79,-34,98,94,-1,-27,28,16,16,0,0,0,0,0,0,0,0,
         // removed the first two bytes
 
         byte[] data = new byte[] {-1,16,0,4,10,0,0,4,16,0,-89,4,48,106,79,-34,98,94,-1,-27,28,16,16,0,0,0,0,0,0,0,0};
         Target target = new Target(0xFF);
-
-        target.parseData(data);
-
-        System.out.println(target.toString());
-        System.out.println(target.getCpuId());
-        System.out.println();
-
-        assertEquals(1008, target.getAvailableFlash());
-        assertEquals(10, target.getBufferPages());
-        assertEquals(1024, target.getFlashPages());
-        assertEquals(255, target.getId());
-        assertEquals(1024, target.getPageSize());
-        assertEquals(16, target.getProtocolVersion()); //0x0F
-        assertEquals(16, target.getStartPage());
+        checkParseData(target, data, 1008, 10, 1024, 255, 1024, 16 /*0x0F*/, 16);
     }
 
     @Test
     public void testParseDataNRF51() {
-
         //original: 1,-1,-2,16,0,4,1,0,-24,0,88,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
         // removed the first two bytes
 
         byte[] data = new byte[] {-2,16,0,4,1,0,-24,0,88,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
         Target target = new Target(0xFE);
+        checkParseData(target, data, 144, 1, 232, 254, 1024, 0 /*0x00*/, 88);
+    }
 
+    private static void checkParseData(Target target, byte[] data, int flash, int buffer, int flashPages, int id, int pageSize, int protocolVersion, int startPage) {
         target.parseData(data);
 
         System.out.println(target.toString());
-        System.out.println(target.getCpuId());
+        System.out.println("CPU ID: " + target.getCpuId());
         System.out.println();
 
-        assertEquals(144, target.getAvailableFlash());
-        assertEquals(1, target.getBufferPages());
-        assertEquals(232, target.getFlashPages());
-        assertEquals(254, target.getId());
-        assertEquals(1024, target.getPageSize());
-        assertEquals(0, target.getProtocolVersion()); //0x00
-        assertEquals(88, target.getStartPage());
+        assertEquals(flash, target.getAvailableFlash());
+        assertEquals(buffer, target.getBufferPages());
+        assertEquals(flashPages, target.getFlashPages());
+        assertEquals(id, target.getId());
+        assertEquals(pageSize, target.getPageSize());
+        assertEquals(protocolVersion, target.getProtocolVersion());
+        assertEquals(startPage, target.getStartPage());
     }
 }
--- src/test/java/se/bitcraze/crazyflie/lib/param/ParamTocElementTest.java
+++ src/test/java/se/bitcraze/crazyflie/lib/param/ParamTocElementTest.java
@@ -45,58 +45,38 @@ public class ParamTocElementTest {
         //UINT8_T
         //original byte: 1,32,0,0,72,105,109,117,95,116,101,115,116,115,0,77,80,85,54,48,53,48,0,0,0,0,0,0,0,0,0,0,0
         byte[] id0 = new byte[] {0,72,105,109,117,95,116,101,115,116,115,0,77,80,85,54,48,53,48,0,0,0,0,0,0,0,0,0,0,0};
-
         ParamTocElement pteId00 = new ParamTocElement(id0);
-
-        assertEquals("imu_tests", pteId00.getGroup());
-        assertEquals("MPU6050", pteId00.getName());
-        assertEquals("imu_tests.MPU6050", pteId00.getCompleteName());
-        assertEquals(VariableType.UINT8_T, pteId00.getCtype());
-        assertEquals(TocElement.RO_ACCESS, pteId00.getAccess());
-        assertEquals("RO", pteId00.getReadableAccess());
+        checkParamTocElement(pteId00, "imu_tests", "MPU6050", VariableType.UINT8_T, TocElement.RO_ACCESS);
         assertEquals(0, pteId00.getIdent());                              //ID can change after firmware update
 
         //UINT16_T
         //original byte: 1,32,0,6,73,99,112,117,0,102,108,97,115,104,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
         byte[] id6 = new byte[] {6,73,99,112,117,0,102,108,97,115,104,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
         ParamTocElement pteId06 = new ParamTocElement(id6);
-
-        assertEquals("cpu", pteId06.getGroup());
-        assertEquals("flash", pteId06.getName());
-        assertEquals("cpu.flash", pteId06.getCompleteName());
-        assertEquals(VariableType.UINT16_T, pteId06.getCtype());
-        assertEquals(TocElement.RO_ACCESS, pteId06.getAccess());
-        assertEquals("RO", pteId06.getReadableAccess());
+        checkParamTocElement(pteId06, "cpu", "flash", VariableType.UINT16_T, TocElement.RO_ACCESS);
         assertEquals(6, pteId06.getIdent());                              //ID can change after firmware update
 
         //UINT32_T
         //original byte: 1,32,0,7,74,99,112,117,0,105,100,48,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
         byte[] id7 = new byte[] {7,74,99,112,117,0,105,100,48,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
         ParamTocElement pteId07 = new ParamTocElement(id7);
-
-        assertEquals("cpu", pteId07.getGroup());
-        assertEquals("id0", pteId07.getName());
-        assertEquals("cpu.id0", pteId07.getCompleteName());
-        assertEquals(VariableType.UINT32_T, pteId07.getCtype());
-        assertEquals(TocElement.RO_ACCESS, pteId07.getAccess());
-        assertEquals("RO", pteId07.getReadableAccess());
+        checkParamTocElement(pteId07, "cpu", "id0", VariableType.UINT32_T, TocElement.RO_ACCESS);
         assertEquals(7, pteId07.getIdent());                              //ID can change after firmware update
 
         //FLOAT
         //original byte: 1,32,0,11,6,112,105,100,95,114,97,116,101,0,114,111,108,108,95,107,112,0,0,0,0,0,0,0,0,0,0,0,0
         byte[] id11 = new byte[] {11,6,112,105,100,95,114,97,116,101,0,114,111,108,108,95,107,112,0,0,0,0,0,0,0,0,0,0,0,0};
-
         ParamTocElement pteId11 = new ParamTocElement(id11);
-
-        assertEquals("pid_rate", pteId11.getGroup());
-        assertEquals("roll_kp", pteId11.getName());
-        assertEquals("pid_rate.roll_kp", pteId11.getCompleteName());
-        assertEquals(VariableType.FLOAT, pteId11.getCtype());
-        assertEquals(TocElement.RW_ACCESS, pteId11.getAccess());
-        assertEquals("RW", pteId11.getReadableAccess());
+        checkParamTocElement(pteId11, "pid_rate", "roll_kp", VariableType.FLOAT, TocElement.RW_ACCESS);
         assertEquals(11, pteId11.getIdent());                              //ID can change after firmware update
+    }
 
+    private static void checkParamTocElement(ParamTocElement pte, String group, String name, VariableType ctype, int access) {
+        assertEquals(group, pte.getGroup());
+        assertEquals(name, pte.getName());
+        assertEquals(group + "." + name, pte.getCompleteName());
+        assertEquals(ctype, pte.getCtype());
+        assertEquals(access, pte.getAccess());
+        assertEquals(access == TocElement.RW_ACCESS ? "RW" : "RO", pte.getReadableAccess());
     }
 }
