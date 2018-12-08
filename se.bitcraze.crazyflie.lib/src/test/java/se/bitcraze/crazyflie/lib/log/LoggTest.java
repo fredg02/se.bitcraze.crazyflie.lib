f95780909c6015b612d865021d459147101cfe4f
Fred G <fredg02@hotmail.com>
2018-12-08 02:30:21 +0100

    Sync with Android Crazyflie lib
    Replace IllegalStateExceptions with IllegalArgumentExceptions

--- se.bitcraze.crazyflie.lib/src/test/java/se/bitcraze/crazyflie/lib/log/LoggTest.java
+++ se.bitcraze.crazyflie.lib/src/test/java/se/bitcraze/crazyflie/lib/log/LoggTest.java
@@ -252,35 +252,35 @@ public class LoggTest {
     }
 
     @Category(OfflineTests.class)
-    @Test(expected = IllegalStateException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testAddConfigNullException() {
          Logg logg = new Logg(new Crazyflie(new MockDriver()));
          logg.addConfig(null);
     }
 
     @Category(OfflineTests.class)
-    @Test(expected = IllegalStateException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testCreateConfigNullException() {
         Logg logg = new Logg(new Crazyflie(new MockDriver()));
         logg.create(null);
     }
 
     @Category(OfflineTests.class)
-    @Test(expected = IllegalStateException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testStartConfigNullException() {
         Logg logg = new Logg(new Crazyflie(new MockDriver()));
         logg.start(null);
     }
 
     @Category(OfflineTests.class)
-    @Test(expected = IllegalStateException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testStopConfigNullException() {
         Logg logg = new Logg(new Crazyflie(new MockDriver()));
         logg.stop(null);
     }
 
     @Category(OfflineTests.class)
-    @Test(expected = IllegalStateException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testDeleteConfigNullException() {
         Logg logg = new Logg(new Crazyflie(new MockDriver()));
         logg.delete(null);
